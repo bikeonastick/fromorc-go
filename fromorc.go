3467e017f096fcf14c8e08ee634c165258279c04
bikeonastick <bikeonastick@gmail.com>
2022-04-01 17:30:19 -0500

    refactored using bubbletea from charm.sh

--- fromorc.go
+++ fromorc.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
+
+const url = "https://api.morcmtb.org/v1/trails"
 
 type Trails []struct {
 	TrailName   string `json:"trailName"`
@@ -25,7 +30,67 @@ type Trails []struct {
 	Street      string `json:"street"`
 }
 
-func TrailStatusEmoji(status string) string {
+type model struct {
+	data Trails
+	err  string
+}
+
+func checkServer() tea.Msg {
+	//currently very happy path centric
+	trails := getTrailObjs(url)
+
+	if len(trails) == 0 {
+		return errMsg{"no trails found"}
+	}
+
+	return dataMsg(trails)
+}
+
+type dataMsg Trails
+
+type errMsg struct{ err string }
+
+func (m model) Init() tea.Cmd {
+	return checkServer
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case dataMsg:
+		m.data = Trails(msg)
+		return m, tea.Quit
+
+	case errMsg:
+		m.err = msg.err
+		return m, tea.Quit
+
+	case tea.KeyMsg:
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.err != "" {
+		return fmt.Sprintf("\nThere was an error: %v\n\n", m.err)
+	}
+
+	s := fmt.Sprintf("checking %s ...", url)
+
+	for _, trail := range m.data {
+		s += fmt.Sprintf(fmt.Sprintf("%s - %s - %s\n", trail.TrailName,
+			trailStatusEmoji(trail.TrailStatus),
+			trailStatusConfidenceEmoji(trail.UpdatedAt)))
+	}
+
+	return "\n" + s + "\n\n"
+}
+
+func trailStatusEmoji(status string) string {
 	if status == "Closed" {
 		return "\xF0\x9F\x91\x8E"
 	} else if status == "Open" {
@@ -36,7 +101,7 @@ func TrailStatusEmoji(status string) string {
 
 }
 
-func TrailStatusConfidenceEmoji(updatedAt int64) string {
+func trailStatusConfidenceEmoji(updatedAt int64) string {
 	updatedTime := time.UnixMilli(updatedAt)
 	var timeSince time.Duration = time.Now().Sub(updatedTime)
 	if timeSince.Hours() <= 48 {
@@ -48,7 +113,7 @@ func TrailStatusConfidenceEmoji(updatedAt int64) string {
 	}
 }
 
-func GetData(url string) []byte {
+func getData(url string) []byte {
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
@@ -70,22 +135,16 @@ func GetData(url string) []byte {
 }
 
 func getTrailObjs(url string) (t Trails) {
-	resp := GetData(url)
-	e2 := json.Unmarshal(resp, &t)
-	if e2 != nil {
-		panic(e2)
-	} else {
-		return
-	}
+	resp := getData(url)
+	/*e2 :=*/ json.Unmarshal(resp, &t)
+	//TODO: bubble possible data errors up to the view
+	return t
 }
 
 func main() {
-	trails := getTrailObjs("https://api.morcmtb.org/v1/trails")
-
-	for _, trail := range trails {
-		fmt.Println(fmt.Sprintf("%s - %s - %s", trail.TrailName,
-			TrailStatusEmoji(trail.TrailStatus),
-			TrailStatusConfidenceEmoji(trail.UpdatedAt)))
+	if err := tea.NewProgram(model{}).Start(); err != nil {
+		fmt.Printf("Uh oh, there was an error: %v\n", err)
+		os.Exit(1)
 	}
 
 }
